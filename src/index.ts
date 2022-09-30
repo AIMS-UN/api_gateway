30b57712cc60a8be5af094dfd0f2dd5bb49bc596
SGman98 <bastiangarnica@gmail.com>
2022-09-30 13:20:55 -0500

    feat: ✨ create basic server using express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-const myName = 'World'
+import 'module-alias/register'
 
-console.log(`Hello ${myName}!`)
+import express from 'express'
+import {
+  PORT,
+  HOST,
+  NODE_ENV
+} from '@configs/index'
+
+// Create a new express application instance
+const app = express()
+
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://${HOST}:${PORT} in ${NODE_ENV} mode`)
+})
