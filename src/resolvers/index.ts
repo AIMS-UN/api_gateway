e90afbd91c7d175816c60c77d08068207886dee4
DavidHernandez2001 <david.hernandez@mercadolibre.com.co>
2022-10-05 08:53:38 -0500

    feat: :bento: create and implementate schedule resolvers and update index resolvers

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,8 +1,10 @@
 import { NonEmptyArray } from 'type-graphql'
 import { PingResolver } from './ping'
 import { GradingResolver } from './grading'
+import { ScheculeResolver } from './schedule'
 
 export const resolvers: NonEmptyArray<Function> | NonEmptyArray<string> = [
   PingResolver,
-  GradingResolver
+  GradingResolver,
+  ScheculeResolver
 ]
