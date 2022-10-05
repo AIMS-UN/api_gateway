3244194a7f401a14ac324335fbaa121df3230e2e
DavidHernandez2001 <david.hernandez@mercadolibre.com.co>
2022-10-05 08:44:52 -0500

    feat: :bento: Create and implementate enrollment schema

---
+++ src/schemas/enrollment.ts
@@ -0,0 +1,22 @@
+import { ID, Field, ObjectType } from 'type-graphql'
+
+@ObjectType()
+export class Enrollment {
+  @Field((type) => ID)
+  id!: number
+
+  @Field()
+  user!: string
+
+  @Field()
+  group!: string
+
+  @Field()
+  subject!: string
+
+  @Field()
+  semester!: string
+
+  @Field({ nullable: true })
+  finalGrade!: number
+}
