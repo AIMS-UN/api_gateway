872ca0450032fbc6a30429799aa44edafbfd88df
Judirale13 <93725600+Judirale13@users.noreply.github.com>
2022-10-05 20:01:45 -0500

    feat: :sparkles: add resolvers schemas and services for subject microservice

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,8 +1,10 @@
 import { NonEmptyArray } from 'type-graphql'
 import { PingResolver } from './ping'
 import { GradingResolver } from './grading'
+import { SubjectResolver } from './subject'
 
 export const resolvers: NonEmptyArray<Function> | NonEmptyArray<string> = [
   PingResolver,
-  GradingResolver
+  GradingResolver,
+  SubjectResolver
 ]
---
+++ src/resolvers/subject.ts
@@ -0,0 +1,25 @@
+import { Subject } from '@/schemas/subject'
+import * as subjectService from '@/services/subject'
+import { Arg, Query, Resolver } from 'type-graphql'
+
+@Resolver()
+export class SubjectResolver {
+  @Query(() => [Subject])
+  async getSubjectbyName (@Arg('name') name: string): Promise<Subject[]> {
+    const a = await subjectService.getSubjectbyName(name)
+    console.log(a)
+    return a
+  }
+
+  @Query(() => [Subject])
+  async getSubjectbyCode (@Arg('code') code: number): Promise<Subject[]> {
+    const a = await subjectService.getSubjectbyCode(code)
+    console.log(a)
+    return a
+  }
+
+  @Query(() => [Subject])
+  async getSubjectbyCareer (@Arg('career') career: number): Promise<Subject[]> {
+    return await subjectService.getSubjectbyCareer(career)
+  }
+}
