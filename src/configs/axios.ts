b4eb6657cfdb007eeefbb4d3ca0ca378da76ef27
SGman98 <bastiangarnica@gmail.com>
2022-10-23 12:04:27 -0500

    chore: 🍱 add axios types and fix vscode config

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
 
 const panic = (message: string): never => { throw new Error(message) }
 
-export const getInstance = (name: string): any => {
+export const getInstance = (name: string): AxiosInstance => {
   const msName = `${name.toUpperCase()}_MS_BASE_URL`
   const baseURL = process.env[msName] ?? panic(`Missing ${msName} environment variable`)
   return axios.create({
